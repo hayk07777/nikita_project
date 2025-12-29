6edfdc0b9cde1e77f55e84c2a555d55e8995ab47
Hayk <navasardyanhayk07777@gmail.com>
2025-12-29 21:05:48 +0400

    project second commite

--- src/components/Footer/contact/ContactWithUs.jsx
+++ src/components/Footer/contact/ContactWithUs.jsx
@@ -1,17 +1,42 @@
-import React from "react";
 import "./ContactWithUs.css";
 import armeniaFlag from "../../../images/picture/flag.png";
 
 export const ContactWithUs = () => {
+  const sendDataBtn = (e) => {
+    const { firstName, lastName, email, telephone, content } = e.target.form;
+
+    e.preventDefault();
+    const NewUser = {
+      id: new Date().getTime().toString(),
+      firstName: firstName.value.trim(),
+      lastName: lastName.value.trim(),
+      email: email.value.trim(),
+      telephone: telephone.value.trim(),
+      content: content.value.trim(),
+    };
+    const postUserData = async () => {
+      const result = await fetch("http://localhost:3005/USER__DATA", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(NewUser),
+      });
+      if (result.ok) {
+        e.target.form.reset();
+      }
+    };
+    postUserData();
+  };
+
   return (
     <section className="contact__section">
       <div className="contact__container">
-        
         <article className="footer__wrapper">
           <div className="title__part">
             <h1>Կապ մեզ հետ</h1>
             <p>
-              Մենք տրամադրում ենք շուրջօրյա ծառայություն, <br /> 
+              Մենք տրամադրում ենք շուրջօրյա ծառայություն, <br />
               խնդրում ենք զանգահարել 010 512 888:
             </p>
           </div>
@@ -20,17 +45,32 @@ export const ContactWithUs = () => {
             <div className="inputs__row">
               <div className="input__item">
                 <p>Անուն*</p>
-                <input type="text" placeholder="Անուն" className="standard__in" />
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="Անուն"
+                  className="standard__in"
+                />
               </div>
               <div className="input__item">
                 <p>Ազգանուն*</p>
-                <input type="text" placeholder="Ազգանուն" className="standard__in" />
+                <input
+                  type="text"
+                  placeholder="Ազգանուն"
+                  name="lastName"
+                  className="standard__in"
+                />
               </div>
             </div>
 
             <div className="input__item">
               <p>Էլ. փոստ*</p>
-              <input type="email" placeholder="Էլ. փոստ" className="standard__in" />
+              <input
+                name="email"
+                type="email"
+                placeholder="Էլ. փոստ"
+                className="standard__in"
+              />
             </div>
 
             <div className="inputs__row">
@@ -42,14 +82,18 @@ export const ContactWithUs = () => {
                     <span>+374</span>
                     <span className="arrow__icon">▼</span>
                   </div>
-                  <input type="tel" placeholder="552123456" />
+                  <input name="telephone" type="tel" placeholder="552123456" />
                 </div>
               </div>
             </div>
 
             <div className="input__item">
               <p>Բովանդակություն*</p>
-              <textarea placeholder="Բովանդակություն" className="textarea__in"></textarea>
+              <textarea
+                name="content"
+                placeholder="Բովանդակություն"
+                className="textarea__in"
+              ></textarea>
             </div>
 
             <div className="services__section">
@@ -74,22 +118,23 @@ export const ContactWithUs = () => {
               </div>
             </div>
 
-            <button type="submit" className="submit__btn">Ուղարկել</button>
+            <button type="submit" className="submit__btn" onClick={sendDataBtn}>
+              Ուղարկել
+            </button>
           </form>
         </article>
 
         <article className="contact__map--part">
           <div className="map__wrapper">
-            <iframe 
-              src="https://yandex.com/map-widget/v1/?feedback=map%2Fedit&feedback-context=map.context&l=sat%2Cskl&ll=43.878471%2C40.380114&z=19" 
-              width="100%" 
-              height="100%" 
+            <iframe
+              src="https://yandex.com/map-widget/v1/?feedback=map%2Fedit&feedback-context=map.context&l=sat%2Cskl&ll=43.878471%2C40.380114&z=19"
+              width="100%"
+              height="100%"
               title="Yandex Map"
               frameBorder="0"
             ></iframe>
           </div>
         </article>
-
       </div>
     </section>
   );
